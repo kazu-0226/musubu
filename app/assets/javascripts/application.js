74e2536a13501127e8cb0559f43978f0d9ac5ca5
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-10 04:23:33 +0000

    [Update]AcitonCableのjs参照先

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,34 +19,34 @@
 //= require toastr
 //= require_tree .
 
-App.chat_room = App.cable.subscriptions.create("ChatRoomChannel", {
-    connected: function() {
-      // Called when the subscription is ready for use on the server
-    },
+// App.chat_room = App.cable.subscriptions.create("ChatRoomChannel", {
+//     connected: function() {
+//       // Called when the subscription is ready for use on the server
+//     },
   
-    disconnected: function() {
-      // Called when the subscription has been terminated by the server
-    },
+//     disconnected: function() {
+//       // Called when the subscription has been terminated by the server
+//     },
   
-    received: function(data) {
-        alert("dfdf")
-      // Called when there's incoming data on the websocket for this channel
-    },
+//     received: function(data) {
+//         alert("dfdf")
+//       // Called when there's incoming data on the websocket for this channel
+//     },
   
-    speak: function(message) {
-        alert(message);
-        return this.perform('speak');
-      }
-    }, $(document).on('keypress', '[data-behavior~=chat_post]', function(event) {
-      if (event.keyCode === 13) {
-        debugger
-        var userForm = $('#user-id-form');
-        var shopForm = $('#shop-id-form');
-        alert(shopForm.val())
-        var chatRoomForm = $('#chat-room-form');
-        alert(chatRoomForm.val())
-        var chatForm = $('#chat-form');
-        App.chat_room.speak(chatForm.val());
-        return chatForm.val('');
-      }
-    }));
+//     speak: function(message) {
+//         alert(message);
+//         return this.perform('speak');
+//       }
+//     }, $(document).on('keypress', '[data-behavior~=chat_post]', function(event) {
+//       if (event.keyCode === 13) {
+//         debugger
+//         var userForm = $('#user-id-form');
+//         var shopForm = $('#shop-id-form');
+//         alert(shopForm.val())
+//         var chatRoomForm = $('#chat-room-form');
+//         alert(chatRoomForm.val())
+//         var chatForm = $('#chat-form');
+//         App.chat_room.speak(chatForm.val());
+//         return chatForm.val('');
+//       }
+//     }));
--- app/assets/javascripts/channels/chat_room.js
+++ app/assets/javascripts/channels/chat_room.js
@@ -1,25 +1,31 @@
 App.chat_room = App.cable.subscriptions.create("ChatRoomChannel", {
-    connected: function() {
-      // Called when the subscription is ready for use on the server
-    },
-  
-    disconnected: function() {
-      // Called when the subscription has been terminated by the server
-    },
-  
-    received: function(data) {
-      // Called when there's incoming data on the websocket for this channel
-    },
-  
-    speak: function(message) {
-        window.alert(message);
-        return this.perform('speak');
-      }
-    }, $(document).on('keypress', '[data-behavior~=chat_post]', function(event) {
-        alert("aaaaa")
-      if (event.keyCode === 13) {
-        var chatForm = $('#chat-form');
-        App.chat.speak(chatForm.val());
-        return chatForm.val('');
-      }
-    }));
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+      alert("dfdf")
+    // Called when there's incoming data on the websocket for this channel
+  },
+
+  speak: function(message) {
+      alert(message);
+      return this.perform('speak');
+    }
+  }, $(document).on('keypress', '[data-behavior~=chat_post]', function(event) {
+    if (event.keyCode === 13) {
+      debugger
+      var userForm = $('#user-id-form');
+      var shopForm = $('#shop-id-form');
+      alert(shopForm.val())
+      var chatRoomForm = $('#chat-room-form');
+      alert(chatRoomForm.val())
+      var chatForm = $('#chat-form');
+      App.chat_room.speak(chatForm.val());
+      return chatForm.val('');
+    }
+  }));
