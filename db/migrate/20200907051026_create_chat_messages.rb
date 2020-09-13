734beb82827977b654722144504b9ae3c85c239b
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-13 05:40:35 +0000

    [change]create_chat_messages.rb

--- db/migrate/20200907051026_create_chat_messages.rb
+++ db/migrate/20200907051026_create_chat_messages.rb
@@ -1,7 +1,7 @@
 class CreateChatMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :chat_messages do |t|
-      t.references :room, foreign_key: true
+      t.references :chat_room, foreign_key: true
       t.references :user, foreign_key: true
       t.references :shop, foreign_key: true
       t.text :content
