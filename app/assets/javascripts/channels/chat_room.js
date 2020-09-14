b708d295a72558b50b9187ae0ed1927e6a0eff50
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-14 05:18:43 +0000

    [Fix]メッセージの表示#24

--- app/assets/javascripts/channels/chat_room.js
+++ app/assets/javascripts/channels/chat_room.js
@@ -9,34 +9,43 @@ App.chat_room = App.cable.subscriptions.create("ChatRoomChannel", {
 
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-      $('#chat_messages').append(data["message"]);
-      debugger
-      // 自分の発言かどうかif分岐
+      // 自分の発言かどうかchat_rooms/show.html.erbのshow_idとsend_idでif分岐
       show_id = $('#show_id').data('show_id')
-      if (data['current_id'] == show_id){ 
-      // class="chat_messages"のchildrenの最後(送信した発言)
-      lastChild = $('#chat_messages').children().last();
-      // chat_boxのleft → rightに変換する
-      lastChild.removeClass('chat-box-left').addClass('chat-box-right')
-      // chat_boxのさらに子要素のleft → rightに変換する
-      lastChild.children().first().removeClass('faceicon-left').addClass('faceicon-right');
-      lastChild.children().last().removeClass('says').addClass('mycomment');
+      if (data['send_id'] == show_id){ 
+        $('#chat_messages').append(data["message_right"])
     }else{
-      lastChild = $('#chat_messages').children().last();
-      // chat_boxのleft → rightに変換する
-      lastChild.removeClass('chat-box-right').addClass('chat-box-left')
-      // chat_boxのさらに子要素のleft → rightに変換する
-      lastChild.children().first().removeClass('faceicon-right').addClass('faceicon-left');
-      lastChild.children().last().removeClass('mycomment').addClass('says');
+        $('#chat_messages').append(data["message_left"])
     }
+    return ;
 
-      
-      return ;
+
+  //   $('#chat_messages').append(data["message"]);
+  //   debugger
+  //   // 自分の発言かどうかif分岐
+  //   if (data['current_id']== $('#chat_messages').children().last().user_id || $('#chat_messages').children().last().shop_id ){ 
+  //   // class="chat_messages"のchildrenの最後(送信した発言)
+  //   lastChild = $('#chat_messages').children().last();
+  //   // chat_boxのleft → rightに変換する
+  //   lastChild.removeClass('chat-box-left').addClass('chat-box-right')
+  //   // chat_boxのさらに子要素のleft → rightに変換する
+  //   lastChild.children().first().removeClass('faceicon-left').addClass('faceicon-right');
+  //   lastChild.children().last().removeClass('says').addClass('mycomment');
+  // }else{
+  //   lastChild = $('#chat_messages').children().last();
+  //   // chat_boxのleft → rightに変換する
+  //   lastChild.removeClass('chat-box-right').addClass('chat-box-left')
+  //   // chat_boxのさらに子要素のleft → rightに変換する
+  //   lastChild.children().first().removeClass('faceicon-right').addClass('faceicon-left');
+  //   lastChild.children().last().removeClass('mycomment').addClass('says');
+  // }
+
+    
+  //   return ;
   },
 
   
-  speak: function(chat_room_id, user_id, shop_id, content, current_id) {
-    return this.perform('speak', { chat_room_id: chat_room_id, user_id: user_id, shop_id: shop_id, content: content, current_id: current_id });
+  speak: function(chat_room_id, user_id, shop_id, content) {
+    return this.perform('speak', { chat_room_id: chat_room_id, user_id: user_id, shop_id: shop_id, content: content });
   
     }
   }, $(document).on('keypress', '[data-behavior~=chat_speaker]', function(event) {
@@ -45,9 +54,7 @@ App.chat_room = App.cable.subscriptions.create("ChatRoomChannel", {
       var shopForm = $('#shop-id-form');
       var chatRoomForm = $('#chat-room-id-form');
       var contentForm = $('#content-form');
-      var currentId = $('#chat-current_id');
-      console.log(currentId)
-      App.chat_room.speak(chatRoomForm.val(),userForm.val(),shopForm.val(),contentForm.val(), currentId.val());
+      App.chat_room.speak(chatRoomForm.val(),userForm.val(),shopForm.val(),contentForm.val());
       //App.chat_room.speak(chatRoomForm.val(),userForm.val(),shopForm.val(),contentForm.val(), currentUserId.val());
       return contentForm.val('');
     }
