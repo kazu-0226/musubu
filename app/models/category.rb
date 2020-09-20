065ec9823cb011be88138734d16acc411533b031
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-20 07:51:59 +0000

    [Add]category_validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,10 @@
 class Category < ApplicationRecord
     has_many :users
     has_many :products
+
+  #バリデーション
+  with_options presence: true,on: :update do
+    validates :code
+    validates :name
+  end
 end
