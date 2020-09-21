8849b8fe861c6bf071679a864508ef476b5c9882
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-21 09:44:04 +0000

    [Update]Top_view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,6 +51,22 @@
 //       }
 //     }));
 
+
+$(function(){
+    var imgHeight = $('.js-mainVisual').outerHeight(); //画像の高さを取得。これがイベント発火位置になる。
+    var header = $('.js-header'); //ヘッダーコンテンツ
+  
+    $(window).on('load scroll', function(){
+       if ($(window).scrollTop() < imgHeight) {
+         //メインビジュアル内にいるので、クラスを外す。
+         header.removeClass('headerColor-default');
+       }else {
+         //メインビジュアルより下までスクロールしたので、クラスを付けて色を変える
+         header.addClass('headerColor-default');
+       }
+    });
+  });
+
 // function addToSearchUrl() {
 //     let path = location.pathname;
 //     let pattern = /users\/search.*$/
