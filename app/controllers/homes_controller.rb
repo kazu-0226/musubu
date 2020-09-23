59d7b2779b6dd4a45d4528592b971710e779ba72
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-23 07:29:05 +0000

    [Add]ゲストログイン

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,12 +3,21 @@ class HomesController < ApplicationController
     @update_shops = Shop.where(is_deleted: false).order('shops.updated_at DESC').limit(3)
   end
 
-  def about
+  def new_guest_user
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
-  def new
-  end
-
-  def login
+  def new_guest_shop
+    shop = Shop.find_or_create_by!(email: 'guest@example.com') do |shop|
+      shop.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in shop
+    redirect_to root_path, notice: 'ゲストショップとしてログインしました。'
   end
 end
