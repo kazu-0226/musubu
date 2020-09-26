747624a557b1a182c73d6b7b9d33eaba8c313019
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-09-26 13:39:49 +0000

    [Add]sign_up,login_test

--- spec/requests/shops_request_spec.rb
+++ spec/requests/shops_request_spec.rb
@@ -2,17 +2,66 @@ require 'rails_helper'
 
 RSpec.describe "Shops", type: :request do
 
-  describe "GET /mypage" do
-    it "returns http success" do
-      get "/shops/shops/mypage"
-      expect(response).to have_http_status(:success)
+  # describe "GET /mypage" do
+  #   it "returns http success" do
+  #     get "/shops/shops/mypage"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET /edit" do
+  #   it "returns http success" do
+  #     get "/shops/shops/edit"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  describe 'お店新規登録' do
+    before do
+      visit new_shop_registration_path
+    end
+    context '新規登録画面に遷移' do
+      it '新規登録に成功する' do
+        fill_in 'shop[email]', with: Faker::Internet.email
+        fill_in 'shop[password]', with: 'password'
+        fill_in 'shop[password_confirmation]', with: 'password'
+        click_button '新規登録'
+
+        expect(page).to have_content '編集'
+      end
+      it '新規登録に失敗する' do
+        fill_in 'shop[email]', with: ''
+        fill_in 'shop[password]', with: ''
+        fill_in 'shop[password_confirmation]', with: ''
+        click_button '新規登録'
+
+        expect(page).to have_content '新規登録'
+      end
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/shops/shops/edit"
-      expect(response).to have_http_status(:success)
+  describe 'お店ログイン' do
+    let(:shop) { create(:shop) }
+    before do
+      visit new_shop_session_path
+    end
+    context 'ログイン画面に遷移' do
+      let(:test_shop) { shop }
+      it 'ログインに成功する' do
+        fill_in 'shop[email]', with: test_shop.email
+        fill_in 'shop[password]', with: test_shop.password
+        click_button 'ログイン'
+
+        expect(page).to have_content '編集'
+      end
+
+      it 'ログインに失敗する' do
+        fill_in 'shop[email]', with: ''
+        fill_in 'shop[password]', with: ''
+        click_button 'ログイン'
+
+        expect(current_path).to eq(new_shop_session_path)
+      end
     end
   end
 
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  let (:id) { '1' }
+
   # describe "GET /show" do
   #   it "returns http success" do
   #     get "/users/users/show"
@@ -16,15 +16,72 @@ RSpec.describe "Users", type: :request do
   #   end
   # end
 
-    describe 'マイページ' do
-      context "マイページが正しく表示される" do
-        before do 
-          get user_path(id)
-        end
-        it 'リクエストは200 OKとなること' do
-          expect(response.status).to eq 200
-        end
+  describe 'ユーザー新規登録' do
+    before do
+      visit new_user_registration_path
+    end
+    context '新規登録画面に遷移' do
+      it '新規登録に成功する' do
+        fill_in 'user[email]', with: Faker::Internet.email
+        fill_in 'user[password]', with: 'password'
+        fill_in 'user[password_confirmation]', with: 'password'
+        click_button '新規登録'
+
+        expect(page).to have_content 'プロフィール編集'
+      end
+      it '新規登録に失敗する' do
+        fill_in 'user[email]', with: ''
+        fill_in 'user[password]', with: ''
+        fill_in 'user[password_confirmation]', with: ''
+        click_button '新規登録'
+
+        expect(page).to have_content '会員新規登録'
       end
     end
+  end
+
+  describe 'ユーザーログイン' do
+    let(:user) { create(:user) }
+    before do
+      visit new_user_session_path
+    end
+    context 'ログイン画面に遷移' do
+      let(:test_user) { user }
+      it 'ログインに成功する' do
+        fill_in 'user[email]', with: test_user.email
+        fill_in 'user[password]', with: test_user.password
+        click_button 'ログイン'
+
+        expect(page).to have_content 'プロフィール編集'
+      end
+
+      it 'ログインに失敗する' do
+        fill_in 'user[email]', with: ''
+        fill_in 'user[password]', with: ''
+        click_button 'ログイン'
+
+        expect(current_path).to eq(new_user_session_path)
+      end
+    end
+  end
+  # describe 'ユーザーログイン' do
+  #   let(:user) { create(:user) }
+  #   user.is_deleted = "true"
+  #   before do
+  #     visit new_user_session_path
+  #   end
+  #   context 'ログイン画面に遷移' do
+  #     let(:test_user) { user }
+  #     it '論理削除したユーザのログイン' do
+  #       fill_in 'user[email]', with: test_user.email
+  #       fill_in 'user[password]', with: test_user.password
+  #       click_button 'ログイン'
+    
+  #       expect(current_path).to eq(new_user_session_path)
+  #     end
+  #   end
+  # end
+
+  
 
 end
