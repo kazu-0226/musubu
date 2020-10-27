1334ef50178a0ba40a758c1f66808efd22ec98ca
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-10-27 12:42:00 +0000

    [Add]メール用データ作成メソッド&ビュー

--- app/controllers/admins/auto_mail_deliveries_controller.rb
+++ app/controllers/admins/auto_mail_deliveries_controller.rb
@@ -1,10 +1,28 @@
 class Admins::AutoMailDeliveriesController < ApplicationController
+  before_action :set_mail, only: [:show, :edit, :update]
+
   def index
+    @auto_mails = AutoMailDelivery.all
+  end
+
+  def show
     
   end
 
   def form
     @auto_mail = AutoMailDelivery.new
+  end
+
+  def edit
+        
+  end
+
+  def update
+    if @auto_mail.update(params[:auto_mail_delivery].permit(:subject, :mail_type, :content, :target_type, :reservation_time))
+      redirect_to admins_auto_mail_delivery_path(@auto_mail)
+    else
+      render :edit
+    end
   end
 
   def confirm
@@ -17,6 +35,12 @@ class Admins::AutoMailDeliveriesController < ApplicationController
   end
 
   def completed
-
+    @auto_mail = AutoMailDelivery.new(params[:auto_mail_delivery].permit(:subject, :mail_type, :content, :target_type, :reservation_time))
+    @auto_mail.save
   end
+
+  def set_mail
+    @auto_mail = AutoMailDelivery.find(params[:id])
+  end
+
 end
