29a35725b2ceccc1ecd8767cef1e1b9d8a4840fe
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-10-31 06:50:16 +0000

    [Fix]アップロードをS3に修正

--- app/uploaders/ckeditor_picture_uploader.rb
+++ app/uploaders/ckeditor_picture_uploader.rb
@@ -9,7 +9,7 @@ class CkeditorPictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
