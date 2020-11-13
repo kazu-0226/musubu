ea8add727cea684165e43ff7517660d1f052aed1
Kazuki Kinebuchi <k.k.5.2.26@i.softbank.jp>
2020-11-13 10:01:40 +0000

    [Add]一覧ページ

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,7 @@
 class NewsController < ApplicationController
   def index
     date = Date.today
-    @news = News.where(open_status: true).where( 'start_date <= ? ', date ).where('end_date >= ? ', date).order('news.start_date DESC')
+    @news = News.where(open_status: true).where( 'start_date <= ? ', date ).where('end_date >= ? ', date).order('news.start_date DESC').page(params[:page]).per(5)
   end
 
   def show
